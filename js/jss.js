b1133a19995fbec21c4e07f338c0b3ad0a6d8800
KenichiKai <147292759+KenichiKai@users.noreply.github.com>
2024-01-13 12:46:18 +0700

    Fix

--- js/jss.js
+++ js/jss.js
@@ -1,3 +1,13 @@
 document.getElementById("signin-btn").addEventListener("click", function () {
     window.location.href = "newfile.html"
 })
+const data = [{
+    name: "Bitexco SkyDeck",
+    image: "../images/29.jpg",
+    about: "Bitexco Financial Tower hay Tháp Tài chính Bitexco là một tòa nhà chọc trời được xây dựng tại trung tâm Quận 1, Thành phố Hồ Chí Minh. Tòa nhà được xây dựng trên diện tích gần 6.100 m². Tổng vốn đầu tư ước tính khoảng 220 triệu USD, được tập đoàn Bitexco Group có trụ sở tại Hà Nội đầu tư xây dựng."
+},
+{
+    name: "",
+    about:""
+}]
+
